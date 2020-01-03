13734e10002bae39f86f3505d65841471b09225b
bz <3910912@gmail.com>
2020-01-03 14:01:23 -0500

    sign in successfully

--- instaTest/signInVC.swift
+++ instaTest/signInVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class signInVC: UIViewController {
 
@@ -23,6 +24,35 @@ class signInVC: UIViewController {
     @IBAction func signInBtn_click(_ sender: Any) {
         
         print("sign in clicked!")
+        self.view.endEditing(true)
+        
+        if usernameTxt.text!.isEmpty || passwordTxt.text!.isEmpty {
+            let alert = UIAlertController(title: "Please", message: "fill in all fields", preferredStyle: UIAlertController.Style.alert)
+            let ok = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+            alert.addAction(ok)
+            self.present(alert, animated: true, completion: nil)
+        }
+        //login function
+        
+        //PFUser.logInWithUsername(inBackground: usernameTxt.text!, password: passwordTxt.text!) { (user:PFUser?, error:NSError?) -> Void in
+        //}
+        PFUser.logInWithUsername(inBackground: usernameTxt.text!, password: passwordTxt.text!) { (user: PFUser?, error:Error?) in
+            if error == nil {
+                   //remember user
+                   
+                   UserDefaults.standard.set(user!.username, forKey: "username")
+                   //UserDefaults.synchronize(UserDefaults)
+                   
+                   //call login function from AppDelegate class
+                   let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+                   appDelegate.login()
+                   
+                   
+               }else {
+                   print(error!.localizedDescription)
+               }
+        }
+        
     }
     
     
