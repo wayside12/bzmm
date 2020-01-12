364fb3e320bdeb1d6f88472eddf8533ef6ce6540
bz <3910912@gmail.com>
2020-01-11 22:50:32 -0500

    editVC finishing except image now shown

--- instaTest/QuickStartExampleApp/editVC.swift
+++ instaTest/QuickStartExampleApp/editVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Parse
 
-class editVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class editVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UIImagePickerControllerDelegate ,UINavigationControllerDelegate {
 
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var avaImg: UIImageView!
@@ -28,24 +28,79 @@ class editVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     var genderPicker : UIPickerView!
     var genders = ["male", "female"]
     
+    //value to hold keyboard frame size
+    var keyboard = CGRect()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         //create picker
         genderPicker = UIPickerView()
-        
         genderPicker.dataSource = self
         genderPicker.delegate = self
-        //genderPicker.showsSelectionIndicator = true
-        //genderPicker.backgroundColor = UIColor.groupTableViewBackground
+        //genderPicker.showsSelectionIndicator = true  //deprecated
+        //genderPicker.backgroundColor = UIColor.groupTableViewBackground  //deprecated
         genderTxt.inputView = genderPicker
         
+        //check keyboard notifications, shown or not
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        //tap keyboard to hide
+        let hideTap = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard(sender:)))
+        hideTap.numberOfTouchesRequired = 1
+        self.view.isUserInteractionEnabled = true
+        self.view.addGestureRecognizer(hideTap)
+        
+        //tap to choose image
+        let imageTap = UITapGestureRecognizer(target: self, action: #selector(loadimg(recognizer:)))
+        imageTap.numberOfTouchesRequired = 1
+        avaImg.isUserInteractionEnabled = true
+        avaImg.addGestureRecognizer(imageTap)
+    
         
         //call alignment
         alignment()
     }
     
+    //func to call UIImagePickerController
+    @objc func loadimg(recognizer:UITapGestureRecognizer) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        avaImg.image = info[UIImagePickerController.InfoKey.editedImage] as! UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func hideKeyboard(sender:Any){
+        self.view.endEditing(true)
+    }
+    
+    @objc func keyboardWillShow(notification:NSNotification) {
+        
+        //keyboard = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey]!.CGRect
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+               let keyboard = keyboardFrame.cgRectValue
+               //let keyboardHeight = keyboard.height
+           }
+        UIView.animate(withDuration: 0.4) {
+            self.scrollView.contentSize.height = self.view.frame.size.height + self.keyboard.height/4
+        }
+                
+    }
+    
+    @objc func keyboardWillHide(notification:NSNotification) {
+        UIView.animate(withDuration: 0.4) {
+            self.scrollView.contentSize.height = 0
+        }
+        
+    }
     
     //alignment
     func alignment()
@@ -54,12 +109,12 @@ class editVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
         let height = self.view.frame.size.height
         
         scrollView.frame = CGRect(x: 0, y: 0, width: width, height: height)
-        avaImg.frame = CGRect(x: width - 68 - 10, y: 15, width: 68, height: 68)
+        avaImg.frame = CGRect(x: width - 68 - 10, y: 15, width: 50, height: 50)
         avaImg.layer.cornerRadius = width / 2
         avaImg.clipsToBounds = true
          
-        fullnameTxt.frame = CGRect(x: 10, y: avaImg.frame.origin.y, width: width - avaImg.frame.size.width  - 30, height: 30)
-        usernameTxt.frame = CGRect(x: 10, y: fullnameTxt.frame.origin.y + 40, width: width - avaImg.frame.size.width - 30, height: 30)
+        fullnameTxt.frame = CGRect(x: 10, y: avaImg.frame.origin.y, width: width - avaImg.frame.size.width  - 50, height: 30)
+        usernameTxt.frame = CGRect(x: 10, y: fullnameTxt.frame.origin.y + 40, width: width - avaImg.frame.size.width - 50, height: 30)
         webTxt.frame = CGRect(x: 10, y: usernameTxt.frame.origin.y + 40, width: width - 20, height: 30)
         
         bioTxt.frame = CGRect(x: 10, y: webTxt.frame.origin.y + 40, width: width - 20, height: 60)
